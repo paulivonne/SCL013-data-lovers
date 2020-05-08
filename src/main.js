48f35647345af4ce05c9ea6a1c3d2e439656fd08
AleKristen <correo@gmail.com>
2020-05-08 19:57:15 -0300

    arreglar codigo

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
 import data from './data/pokemon/pokemon.js';
-import { varType, sortArray} from './data.js';
+import { varType, sortArray, searchText} from './data.js';
+
+/*IMPRIME POKEDEX EN HTML*/
+
+document.getElementById("screen2").style.display="none";
+
+document.getElementById("textIcon1").addEventListener("click", ()=>{
+  document.getElementById("screen2").style.display="block";
+  document.getElementById("screen1").style.display="none";
+});
+
+document.getElementById("menu1").addEventListener("click", ()=>{
+  document.getElementById("screen1").style.display="block";
+  document.getElementById("screen1").style.display="";
+  document.getElementById("screen2").style.display="none";
+
+});
+
+
+
+
+
+
+
 
 /*IMPRIME POKEDEX EN HTML*/
 let pokedata = data.pokemon;
 
-const container = document.getElementById("root");
+const showPokedex = document.getElementById("root");
 
 for (let i = 0; i < pokedata.length; i++){
-    container.innerHTML +=`<div class="pokemon">
+  showPokedex.innerHTML +=`<div class="pokemon">
                           <h1 class="namePoke">${pokedata[i].name}</h1>
                           <p class="numPoke">${pokedata[i].num}</p>
                           <img class="imgPoke" src="${pokedata[i].img}">
@@ -21,7 +44,7 @@ const filter = document.getElementById ("filterType");
 
 filter.addEventListener ("change", () => {
   let selectType = filter.options[filter.selectedIndex].value
-  container.innerHTML="";
+  showPokedex.innerHTML="";
   let searchType = varType (pokedata,'type', selectType);
   for (let i= 0; i <searchType.length; i++){ 
     let findType = searchType[i];
@@ -30,21 +53,34 @@ filter.addEventListener ("change", () => {
                   <p class="numPoke"> ${findType.num}<p>
                   <img class="imgPoke"src=${findType.img} >
                   </div>`
-    container.innerHTML += `<p>${showType}<p>`
+    showPokedex.innerHTML += `<p>${showType}<p>`
     }
     });
 
 //----------------------------------------------------------------------------
-
-
-
 //FILTRAR POR ORDENAR
 
 const orderArray = document.querySelector('#filterOrder');
 orderArray.addEventListener('change', () => {
   const orderSelect = orderArray.value;
-  container.innerHTML = '';
-  container(sortArray(pokeData, orderSelect));
+  showPokedex.innerHTML = '';
+  showPokedex(sortArray(pokeData, orderSelect));
+});
+
+//----------------------------------------------------------------------------
+//SEARCH
+
+const inputText = document.getElementById('#pokeSearch');
+inputText.addEventListener('keyup', () => {
+  const inputTextPkm = inputText.value.toLowerCase();
+  showPokedex(searchText(pokedata, 'name', inputTextPkm));
+  if (showPokedex.innerHTML === '') {
+    showPokedex.innerHTML = `
+    <section class="errorText">
+      <p class="text1"> Ningún POKÉMON coincide con tu búsqueda</p>
+    </section>
+    `;
+  }
 });
 
 
